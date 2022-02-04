683660fb34d7d46ff57676684d4cfe9b721c5289
pixeldev <pixeldev1729@gmail.com>
2022-02-04 12:47:35 -0600

    build: add process resources to replace version token

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,3 +1,13 @@
+tasks {
+    processResources {
+        filesMatching("**/*.yml") {
+            filter<org.apache.tools.ant.filters.ReplaceTokens>(
+                "tokens" to mapOf("version" to project.version)
+            )
+        }
+    }
+}
+
 dependencies {
     api(project(":api"))
     compileOnly("org.spigotmc:spigot-api:1.17.1-R0.1-SNAPSHOT")
