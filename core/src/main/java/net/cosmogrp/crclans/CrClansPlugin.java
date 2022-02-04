58ec76cdd03e41a4adec9c3d25e6be495aff36f9
pixeldev <pixeldev1729@gmail.com>
2022-02-04 12:46:06 -0600

    chore: implement core-plugin structure

---
+++ core/src/main/java/net/cosmogrp/crclans/CrClansPlugin.java
@@ -0,0 +1,17 @@
+package net.cosmogrp.crclans;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CrClansPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
