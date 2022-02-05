0be99f2fcbafac88ae143409e3f1e46506cc1a45
pixeldev <pixeldev1729@gmail.com>
2022-02-04 18:57:06 -0600

    feat(redis): create connection and api

---
+++ core/src/main/java/net/cosmogrp/crclans/inject/RedisModule.java
@@ -0,0 +1,45 @@
+package net.cosmogrp.crclans.inject;
+
+import com.google.gson.Gson;
+import me.yushust.inject.AbstractModule;
+import me.yushust.inject.Provides;
+import net.cosmogrp.crclans.redis.GsonRedis;
+import net.cosmogrp.crclans.redis.JedisBuilder;
+import net.cosmogrp.crclans.redis.JedisInstance;
+import net.cosmogrp.crclans.redis.Redis;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.Plugin;
+
+import javax.inject.Singleton;
+
+public class RedisModule extends AbstractModule {
+
+    @Provides @Singleton
+    public Redis getJedis(Plugin plugin, Gson gson) {
+        FileConfiguration configuration = plugin.getConfig();
+        ConfigurationSection redisSection =
+                configuration.getConfigurationSection("redis");
+
+        JedisInstance jedisInstance;
+
+        if (redisSection == null) {
+            jedisInstance = JedisBuilder.builder()
+                    .setTimeout(2000)
+                    .setHost("localhost")
+                    .setPort(6379)
+                    .setPassword("")
+                    .build();
+        } else {
+            jedisInstance = JedisBuilder.fromConfig(redisSection)
+                    .build();
+        }
+
+        return GsonRedis.builder(plugin)
+                .setParentChannel(configuration.getString("server-group") + "-crclans")
+                .setGson(gson)
+                .setJedis(jedisInstance)
+                .build();
+    }
+
+}
