b6778525bbeeddc8e956eb14ddd3b724f74acfc9
pixeldev <pixeldev1729@gmail.com>
2022-02-05 00:08:09 -0600

    deps: remove hikari cp and include sql-dist

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,6 +1,6 @@
 dependencies {
     api("org.jetbrains:annotations:22.0.0")
-    api("com.zaxxer:HikariCP:5.0.1")
+    api("net.cosmogrp:storage-sql-dist:0.1.0")
     api("redis.clients:jedis:4.1.1")
     api("me.yushust.inject:core:0.4.5-SNAPSHOT")
     compileOnly("org.spigotmc:spigot-api:1.17.1-R0.1-SNAPSHOT")
