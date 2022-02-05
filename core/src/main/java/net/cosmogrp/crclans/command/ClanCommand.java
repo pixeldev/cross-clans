18467a010f73b702459140af564dfea7d739515d
pixeldev <pixeldev1729@gmail.com>
2022-02-05 17:59:13 -0600

    feat(command): create main clan command

---
+++ core/src/main/java/net/cosmogrp/crclans/command/ClanCommand.java
@@ -0,0 +1,8 @@
+package net.cosmogrp.crclans.command;
+
+import me.fixeddev.commandflow.annotated.CommandClass;
+import me.fixeddev.commandflow.annotated.annotation.Command;
+
+@Command(names = {"clan", "clans"})
+public class ClanCommand implements CommandClass {
+}
