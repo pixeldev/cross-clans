8b4df8bc282bbe3fc7cf7f0a49c9db26f488fea8
pixeldev <pixeldev1729@gmail.com>
2022-02-05 01:38:43 -0600

    deps: implement common-bukkit dependencies

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -2,6 +2,11 @@ dependencies {
     api("org.jetbrains:annotations:22.0.0")
     api("net.cosmogrp:storage-sql-dist:0.1.0")
     api("redis.clients:jedis:4.1.1")
+
     api("me.yushust.inject:core:0.4.5-SNAPSHOT")
+    api("me.yushust.message:core:6.0.17")
+    api("me.yushust.message:sourcetype-bukkit-yml:6.0.17")
+    api("me.fixeddev:commandflow-brigadier:0.5.0-SNAPSHOT")
+
     compileOnly("org.spigotmc:spigot-api:1.17.1-R0.1-SNAPSHOT")
 }
