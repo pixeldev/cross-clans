78bd7352bacbcd810a4df9e2f291ebea826f158c
pixeldev <pixeldev1729@gmail.com>
2022-02-09 10:59:12 -0600

    feat(user): add clan data

--- api/src/main/java/net/cosmogrp/crclans/user/User.java
+++ api/src/main/java/net/cosmogrp/crclans/user/User.java
@@ -1,13 +1,17 @@
 package net.cosmogrp.crclans.user;
 
+import net.cosmogrp.crclans.clan.Clan;
 import net.cosmogrp.storage.model.AbstractModel;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.UUID;
 
 public class User extends AbstractModel {
 
     private final UUID playerId;
+
+    private String clanTag;
 
     public User(Player player) {
         super(player.getUniqueId().toString());
@@ -16,5 +20,21 @@ public class User extends AbstractModel {
 
     public UUID getPlayerId() {
         return playerId;
+    }
+
+    public void setClan(@Nullable Clan clan) {
+        if (clan == null) {
+            this.clanTag = null;
+        } else {
+            this.clanTag = clan.getId();
+        }
+    }
+
+    public @Nullable String getClanTag() {
+        return clanTag;
+    }
+
+    public boolean hasClan() {
+        return clanTag != null;
     }
 }
