dcf0099596e3221a668304f88d9bd8b17a7f787e
pixeldev <pixeldev1729@gmail.com>
2022-02-10 09:01:41 -0600

    feat(user): use AsyncPlayerPreLoginEvent to load user

--- core/src/main/java/net/cosmogrp/crclans/listener/PlayerConnectionListener.java
+++ core/src/main/java/net/cosmogrp/crclans/listener/PlayerConnectionListener.java
@@ -2,11 +2,13 @@ package net.cosmogrp.crclans.listener;
 
 import com.google.common.io.ByteArrayDataOutput;
 import com.google.common.io.ByteStreams;
+import net.cosmogrp.crclans.user.UserService;
 import net.cosmogrp.storage.redis.connection.RedisCache;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.Plugin;
@@ -16,7 +18,17 @@ import javax.inject.Inject;
 public class PlayerConnectionListener implements Listener {
 
     @Inject private RedisCache redisCache;
+    @Inject private UserService userService;
     @Inject private Plugin plugin;
+
+    @EventHandler
+    public void onLogin(AsyncPlayerPreLoginEvent event) {
+        String kickReason = userService.loadOrCreate(event.getUniqueId());
+
+        if (kickReason != null) {
+            event.disallow(AsyncPlayerPreLoginEvent.Result.KICK_OTHER, kickReason);
+        }
+    }
 
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
