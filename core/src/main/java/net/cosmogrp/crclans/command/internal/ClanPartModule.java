e729ee91110126f036fb5175b9cb8d3be3bbd066
pixeldev <pixeldev1729@gmail.com>
2022-02-13 14:41:31 -0600

    feat(command): register user parts

--- core/src/main/java/net/cosmogrp/crclans/command/internal/ClanPartModule.java
+++ core/src/main/java/net/cosmogrp/crclans/command/internal/ClanPartModule.java
@@ -1,17 +1,26 @@
 package net.cosmogrp.crclans.command.internal;
 
 import me.fixeddev.commandflow.annotated.part.AbstractModule;
+import me.fixeddev.commandflow.annotated.part.Key;
+import me.fixeddev.commandflow.bukkit.annotation.Sender;
 import net.cosmogrp.crclans.command.part.ClusteredUserPart;
+import net.cosmogrp.crclans.command.part.UserPart;
+import net.cosmogrp.crclans.command.part.UserSenderPart;
 import net.cosmogrp.crclans.user.ClusteredUser;
+import net.cosmogrp.crclans.user.User;
 
 import javax.inject.Inject;
 
 public class ClanPartModule extends AbstractModule {
 
     @Inject private ClusteredUserPart clusteredUserPart;
+    @Inject private UserPart userPart;
+    @Inject private UserSenderPart userSenderPart;
 
     @Override
     public void configure() {
         bindFactory(ClusteredUser.class, clusteredUserPart);
+        bindFactory(User.class, userPart);
+        bindFactory(new Key(User.class, Sender.class), userSenderPart);
     }
 }
