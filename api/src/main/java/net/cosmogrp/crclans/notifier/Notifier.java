62c2cc427fec340c28ff0993185c4f7ab0f93217
pixeldev <pixeldev1729@gmail.com>
2022-02-13 21:36:22 -0600

    feat(notifier): implement method to single notify

--- api/src/main/java/net/cosmogrp/crclans/notifier/Notifier.java
+++ api/src/main/java/net/cosmogrp/crclans/notifier/Notifier.java
@@ -1,5 +1,6 @@
 package net.cosmogrp.crclans.notifier;
 
+import com.google.common.collect.Sets;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Set;
@@ -8,6 +9,10 @@ import java.util.UUID;
 public interface Notifier {
 
     void notify(@Nullable Set<UUID> targets, String path, Object... parameters);
+
+    default void singleNotify(UUID target, String path, Object... parameters) {
+        notify(Sets.newHashSet(target), path, parameters);
+    }
 
     default void notify(String path, Object... parameters) {
         notify(null, path, parameters);
