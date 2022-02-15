feb1ee61c1ebcb0ed6b9a0dafc6ed6f5c8259ce1
pixeldev <pixeldev1729@gmail.com>
2022-02-15 15:26:35 -0600

    refactor(translation): implement it into it's corresponding module

---
+++ adapt/adapt-v1_16_R3/src/main/java/net/cosmogrp/crclans/adapt/v1_16_R3/AdaptionModule1_16_R3.java
@@ -0,0 +1,41 @@
+package net.cosmogrp.crclans.adapt.v1_16_R3;
+
+import me.yushust.inject.AbstractModule;
+import me.yushust.inject.Provides;
+import me.yushust.message.send.MessageSender;
+import net.kyori.adventure.platform.bukkit.BukkitAudiences;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
+import javax.inject.Singleton;
+
+import static org.bukkit.ChatColor.translateAlternateColorCodes;
+
+public class AdaptionModule1_16_R3 extends AbstractModule {
+
+    @Provides @Singleton
+    public MessageSender<CommandSender> createMessageSender(Plugin plugin) {
+        return new MessageSender<>() {
+
+            private final BukkitAudiences audiences =
+                    BukkitAudiences.create(plugin);
+
+            @Override
+            public void send(CommandSender sender, String mode, String message) {
+                if (mode.equals("minimessage") &&
+                        sender instanceof Player) {
+                    audiences.player((Player) sender)
+                            .sendMessage(MiniMessage.miniMessage()
+                                    .deserialize(message));
+                } else {
+                    sender.sendMessage(translateAlternateColorCodes(
+                            '&', message
+                    ));
+                }
+            }
+        };
+    }
+
+}
--- core/src/main/java/net/cosmogrp/crclans/translate/TranslationModule.java
+++ core/src/main/java/net/cosmogrp/crclans/translate/TranslationModule.java
@@ -4,46 +4,26 @@ import me.yushust.inject.AbstractModule;
 import me.yushust.inject.Provides;
 import me.yushust.message.MessageHandler;
 import me.yushust.message.bukkit.BukkitMessageAdapt;
-import net.kyori.adventure.platform.bukkit.BukkitAudiences;
-import net.kyori.adventure.text.minimessage.MiniMessage;
+import me.yushust.message.send.MessageSender;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 import javax.inject.Singleton;
 
-import static org.bukkit.ChatColor.translateAlternateColorCodes;
-
 public class TranslationModule extends AbstractModule {
-
-    @Provides @Singleton
-    public BukkitAudiences createAudience(Plugin plugin) {
-        return BukkitAudiences.create(plugin);
-    }
 
     @Provides
     @Singleton
     public MessageHandler getMessageHandler(
             Plugin plugin,
-            BukkitAudiences audiences
+            MessageSender<CommandSender> messageSender
     ) {
         return MessageHandler.of(
                 BukkitMessageAdapt.newYamlSource(plugin),
                 configurationHandle -> {
                     configurationHandle.specify(CommandSender.class)
                             .setLinguist(sender -> "es")
-                            .setMessageSender((sender, mode, message) -> {
-                                if (mode.equals("minimessage") &&
-                                        sender instanceof Player) {
-                                    audiences.player((Player) sender)
-                                            .sendMessage(MiniMessage.miniMessage()
-                                                    .deserialize(message));
-                                } else {
-                                    sender.sendMessage(translateAlternateColorCodes(
-                                            '&', message
-                                    ));
-                                }
-                            });
+                            .setMessageSender(messageSender);
                 }
         );
     }
