8f7623054e3bbcf28c360156782930a57ef228b0
pixeldev <pixeldev1729@gmail.com>
2022-02-16 14:20:55 -0600

    feat(clan): implement clan kick logic

--- api/src/main/java/net/cosmogrp/crclans/user/clan/ClanUserService.java
+++ api/src/main/java/net/cosmogrp/crclans/user/clan/ClanUserService.java
@@ -1,10 +1,12 @@
 package net.cosmogrp.crclans.user.clan;
 
 import net.cosmogrp.crclans.clan.Clan;
+import net.cosmogrp.crclans.clan.ClanMember;
 import net.cosmogrp.crclans.user.User;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.UUID;
 import java.util.function.Consumer;
 
 public interface ClanUserService {
@@ -14,6 +16,13 @@ public interface ClanUserService {
     void disconnect(User user);
 
     @Nullable Clan getClan(Player player, User user);
+
+    void kickMember(
+            Player player, User user,
+            Clan clan, ClanMember target
+    );
+
+    boolean notifyKick(UUID targetId, String clanId);
 
     void disbandClan(Player player, User user);
 
--- core/src/main/java/net/cosmogrp/crclans/user/clan/SimpleClanUserService.java
+++ core/src/main/java/net/cosmogrp/crclans/user/clan/SimpleClanUserService.java
@@ -1,4 +1,4 @@
-package net.cosmogrp.crclans.user;
+package net.cosmogrp.crclans.user.clan;
 
 import me.yushust.message.MessageHandler;
 import net.cosmogrp.crclans.clan.Clan;
@@ -6,22 +6,31 @@ import net.cosmogrp.crclans.clan.ClanMember;
 import net.cosmogrp.crclans.clan.ClanService;
 import net.cosmogrp.crclans.log.LogHandler;
 import net.cosmogrp.crclans.notifier.global.GlobalNotifier;
-import net.cosmogrp.crclans.user.clan.ClanUserService;
+import net.cosmogrp.crclans.user.User;
+import net.cosmogrp.crclans.user.UserService;
+import net.cosmogrp.crclans.user.cluster.ClusteredUser;
+import net.cosmogrp.crclans.user.cluster.ClusteredUserRegistry;
 import net.cosmogrp.storage.AsyncModelService;
+import net.cosmogrp.storage.redis.channel.Channel;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.Nullable;
 
 import javax.inject.Inject;
+import java.util.UUID;
 import java.util.function.Consumer;
 
 public class SimpleClanUserService
         implements ClanUserService {
 
     @Inject private ClanService clanService;
+    @Inject private UserService userService;
     @Inject private MessageHandler messageHandler;
     @Inject private GlobalNotifier globalNotifier;
     @Inject private AsyncModelService<Clan> modelService;
+    @Inject private ClusteredUserRegistry clusteredUserRegistry;
     @Inject private LogHandler logHandler;
+    @Inject private Channel<ClanKickMessage> kickChannel;
 
     @Override
     public @Nullable Clan getClan(Player player, User user) {
@@ -37,6 +46,76 @@ public class SimpleClanUserService
         }
 
         return clan;
+    }
+
+    @Override
+    public void kickMember(
+            Player player, User user,
+            Clan clan, ClanMember target
+    ) {
+        ClanMember clanMember = clan.getMember(user.getPlayerId());
+
+        if (clanMember == null) {
+            // this should never happen
+            return;
+        }
+
+        if (!clanMember.isModerator()) {
+            messageHandler.send(player, "clan.not-mod");
+            return;
+        }
+
+        if (target.isModerator()) {
+            messageHandler.send(player, "clan.kick-mod");
+            return;
+        }
+
+        UUID targetId = target.getPlayerId();
+        clan.removeMember(targetId);
+        messageHandler.send(player, "clan.kick-success-sender");
+        globalNotifier.notify(
+                clan.getOnlineMembers(), "clan.kick-success-members",
+                "%sender%", player.getName(),
+                "%target%", target.getPlayerName()
+        );
+
+        ClusteredUser clusteredTarget =
+                clusteredUserRegistry.find(target.getPlayerName());
+
+        if (clusteredTarget != null) {
+            String clanTag = clan.getId();
+            if (notifyKick(targetId, clanTag)) {
+                return;
+            }
+
+            kickChannel.sendMessage(
+                    new ClanKickMessage(targetId, clanTag),
+                    clusteredTarget.getServerData().getRedisServer()
+            );
+        }
+    }
+
+    @Override
+    public boolean notifyKick(UUID targetId, String clanId) {
+        Player player = Bukkit.getPlayer(targetId);
+
+        if (player == null) {
+            return false;
+        }
+
+        User user = userService.getUser(player);
+
+        if (user == null) {
+            return false;
+        }
+
+        user.setClan(null);
+        messageHandler.sendReplacing(
+                player, "clan.kick-success-target",
+                "%tag%", clanId
+        );
+
+        return true;
     }
 
     @Override
