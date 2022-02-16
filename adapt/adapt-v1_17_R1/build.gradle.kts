5c344c9f30b6e550ba3ae433f2547c6a3aae5495
pixeldev <pixeldev1729@gmail.com>
2022-02-15 19:43:30 -0600

    deps: use a lower minimessage version

--- adapt/adapt-v1_17_R1/build.gradle.kts
+++ adapt/adapt-v1_17_R1/build.gradle.kts
@@ -1,5 +1,9 @@
 dependencies {
     compileOnly(project(":adapt"))
-    implementation("net.kyori:adventure-text-minimessage:4.10.0-SNAPSHOT")
+
+    implementation("net.kyori:adventure-text-minimessage:4.2.0-SNAPSHOT") {
+        exclude("net.kyori", "adventure-api")
+    }
+
     compileOnly("io.papermc.paper:paper-api:1.17.1-R0.1-SNAPSHOT")
 }
