f2426dc9bfd7f58e7c2528c9d98df4af593c80e4
pixeldev <pixeldev1729@gmail.com>
2022-02-23 01:56:22 -0600

    !refactor(user): remove unused methods and adapt to micro-services

--- core/src/main/java/net/cosmogrp/crclans/user/clan/SimpleClanUserService.java
+++ core/src/main/java/net/cosmogrp/crclans/user/clan/SimpleClanUserService.java
@@ -1,39 +1,22 @@
 package net.cosmogrp.crclans.user.clan;
 
 import me.yushust.message.MessageHandler;
-import net.cosmogrp.crclans.clan.Clan;
 import net.cosmogrp.crclans.clan.member.ClanMember;
-import net.cosmogrp.crclans.clan.ClanService;
+import net.cosmogrp.crclans.clan.member.ClanMemberData;
+import net.cosmogrp.crclans.clan.member.ClanMemberService;
 import net.cosmogrp.crclans.log.LogHandler;
 import net.cosmogrp.crclans.user.User;
 import org.bukkit.entity.Player;
-import org.jetbrains.annotations.Nullable;
 
 import javax.inject.Inject;
-import java.util.function.Consumer;
 
 public class SimpleClanUserService
         implements ClanUserService {
 
-    @Inject private ClanService clanService;
+    @Inject private ClanMemberService memberService;
+
     @Inject private MessageHandler messageHandler;
     @Inject private LogHandler logHandler;
-
-    @Override
-    public @Nullable Clan getClan(Player player, User user) {
-        if (!user.hasClan()) {
-            messageHandler.send(player, "clan.not-in-clan");
-            return null;
-        }
-
-        Clan clan = clanService.getClan(user.getClanTag());
-
-        if (clan == null) {
-            messageHandler.send(player, "clan.error-finding-clan");
-        }
-
-        return clan;
-    }
 
     @Override
     public void connect(Player player, User user) {
@@ -43,18 +26,18 @@ public class SimpleClanUserService
             return;
         }
 
-        Clan clan = clanService.getClan(clanTag);
+        ClanMemberData memberData = memberService.getData(clanTag);
 
         // in most cases this happens if clan has
         // been disbanded and player wasn't
         // online when it happened
-        if (clan == null) {
+        if (memberData == null) {
             user.setClan(null);
             messageHandler.send(player, "clan.disband-offline-notify");
             return;
         }
 
-        ClanMember member = clan.getMember(user.getPlayerId());
+        ClanMember member = memberData.getMember(user.getPlayerId());
 
         // in most cases this happens if player
         // has been kicked from clan
@@ -65,7 +48,7 @@ public class SimpleClanUserService
         }
 
         member.setOnline(true);
-        clanService.saveClan(clan);
+        memberService.save(player, memberData);
     }
 
     @Override
@@ -76,19 +59,16 @@ public class SimpleClanUserService
             return;
         }
 
-        Clan clan = clanService.getClan(clanTag);
+        ClanMemberData memberData = memberService.getData(clanTag);
 
-        if (clan == null) {
-            logHandler.reportError(
-                    "Failed to find clan '%s' in user save",
-                    clanTag
-            );
+        if (memberData == null) {
             return;
         }
 
-        ClanMember member = clan.getMember(user.getPlayerId());
+        ClanMember member = memberData.getMember(user.getPlayerId());
 
         if (member == null) {
+            // this should never happen
             logHandler.reportError(
                     "Failed to find member '%s' in clan '%s'",
                     user.getPlayerId().toString(), clanTag
@@ -97,42 +77,6 @@ public class SimpleClanUserService
         }
 
         member.setOnline(false);
-        clanService.saveClan(clan);
-    }
-
-    @Override
-    public void executeAsOwner(
-            Player player, User user,
-            Consumer<Clan> consumer
-    ) {
-        executeAsOwner0(player, user, consumer);
-    }
-
-    @Override
-    public void computeAsOwner(Player player, User user, Consumer<Clan> consumer) {
-        Clan clan = executeAsOwner0(player, user, consumer);
-
-        if (clan != null) {
-            clanService.saveClan(player, clan);
-        }
-    }
-
-    private @Nullable Clan executeAsOwner0(
-            Player player, User user,
-            Consumer<Clan> consumer
-    ) {
-        Clan clan = getClan(player, user);
-
-        if (clan == null) {
-            return null;
-        }
-
-        if (!clan.isOwner(player)) {
-            messageHandler.send(player, "clan.not-owner");
-            return null;
-        }
-
-        consumer.accept(clan);
-        return clan;
+        memberService.save(memberData);
     }
 }
