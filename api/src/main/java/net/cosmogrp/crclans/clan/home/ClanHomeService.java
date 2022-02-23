2ca67d8870bb381ba3018919bfc81df8610fe78e
pixeldev <pixeldev1729@gmail.com>
2022-02-23 01:27:12 -0600

    feat(clan): extend from ClanService

--- api/src/main/java/net/cosmogrp/crclans/clan/home/ClanHomeService.java
+++ api/src/main/java/net/cosmogrp/crclans/clan/home/ClanHomeService.java
@@ -1,9 +1,11 @@
 package net.cosmogrp.crclans.clan.home;
 
+import net.cosmogrp.crclans.clan.ClanService;
 import net.cosmogrp.crclans.user.User;
 import org.bukkit.entity.Player;
 
-public interface ClanHomeService {
+public interface ClanHomeService
+        extends ClanService<ClanHomeData> {
 
     void setHome(Player player, User user);
 
