66248a70e73eadbb7d74ca2d5d5533a11fc01843
pixeldev <pixeldev1729@gmail.com>
2022-02-23 11:58:15 -0600

    refactor(service): bind with abstractions

--- core/src/main/java/net/cosmogrp/crclans/clan/service/ClanServicesLoader.java
+++ core/src/main/java/net/cosmogrp/crclans/clan/service/ClanServicesLoader.java
@@ -2,13 +2,13 @@ package net.cosmogrp.crclans.clan.service;
 
 import net.cosmogrp.crclans.CrClansPlugin;
 import net.cosmogrp.crclans.clan.ClanData;
-import net.cosmogrp.crclans.clan.SimpleClanDataService;
+import net.cosmogrp.crclans.clan.ClanDataService;
 import net.cosmogrp.crclans.clan.home.ClanHomeData;
-import net.cosmogrp.crclans.clan.home.SimpleClanHomeService;
+import net.cosmogrp.crclans.clan.home.ClanHomeService;
 import net.cosmogrp.crclans.clan.member.ClanMemberData;
-import net.cosmogrp.crclans.clan.member.SimpleClanMemberService;
+import net.cosmogrp.crclans.clan.member.ClanMemberService;
 import net.cosmogrp.crclans.clan.recruitment.ClanRecruitmentData;
-import net.cosmogrp.crclans.clan.recruitment.SimpleClanRecruitmentService;
+import net.cosmogrp.crclans.clan.recruitment.ClanRecruitmentService;
 import net.cosmogrp.crclans.loader.Loader;
 
 import javax.inject.Inject;
@@ -19,9 +19,9 @@ public class ClanServicesLoader implements Loader {
 
     @Override
     public void load() {
-        plugin.registerService(ClanData.class, SimpleClanDataService.class);
-        plugin.registerService(ClanMemberData.class, SimpleClanMemberService.class);
-        plugin.registerService(ClanRecruitmentData.class, SimpleClanRecruitmentService.class);
-        plugin.registerService(ClanHomeData.class, SimpleClanHomeService.class);
+        plugin.registerService(ClanData.class, ClanDataService.class);
+        plugin.registerService(ClanMemberData.class, ClanMemberService.class);
+        plugin.registerService(ClanRecruitmentData.class, ClanRecruitmentService.class);
+        plugin.registerService(ClanHomeData.class, ClanHomeService.class);
     }
 }
