b39c45dfd09b664d2d4df0486e5662c7764fbe86
pixeldev <pixeldev1729@gmail.com>
2022-02-24 12:25:44 -0600

    refactor(clan): just add a single method to change user channel

--- api/src/main/java/net/cosmogrp/crclans/clan/chat/ClanChatService.java
+++ api/src/main/java/net/cosmogrp/crclans/clan/chat/ClanChatService.java
@@ -1,12 +1,14 @@
 package net.cosmogrp.crclans.clan.chat;
 
+import net.cosmogrp.crclans.clan.channel.ClanChannel;
 import net.cosmogrp.crclans.user.User;
 import org.bukkit.entity.Player;
 
 public interface ClanChatService {
 
-    void setClanChat(Player player, User user);
-
-    void setAlliesChat(Player player, User user);
+    void setChannel(
+            Player player, User user,
+            ClanChannel clanChannel
+    );
 
 }
