d5761178a7742ec01e1cd8c48a647fee1b1f35e7
pixeldev <pixeldev1729@gmail.com>
2022-02-26 16:45:19 -0600

    feat(clan-enemy): implement clan enemy service

---
+++ api/src/main/java/net/cosmogrp/crclans/clan/enemy/ClanEnemyService.java
@@ -0,0 +1,20 @@
+package net.cosmogrp.crclans.clan.enemy;
+
+import net.cosmogrp.crclans.clan.member.ClanMemberData;
+import net.cosmogrp.crclans.clan.service.DomainClanService;
+import net.cosmogrp.crclans.user.User;
+import org.bukkit.entity.Player;
+
+public interface ClanEnemyService extends DomainClanService<ClanEnemyData> {
+
+    void addEnemy(
+            Player sender, User user,
+            ClanMemberData targetMemberData
+    );
+
+    void removeEnemy(
+            Player sender, User user,
+            ClanMemberData targetMemberData
+    );
+
+}
---
+++ core/src/main/java/net/cosmogrp/crclans/clan/enemy/SimpleClanEnemyService.java
@@ -0,0 +1,102 @@
+package net.cosmogrp.crclans.clan.enemy;
+
+import net.cosmogrp.crclans.clan.member.ClanMemberData;
+import net.cosmogrp.crclans.clan.service.AbstractDomainClanService;
+import net.cosmogrp.crclans.notifier.global.GlobalNotifier;
+import net.cosmogrp.crclans.user.User;
+import org.bukkit.entity.Player;
+
+import javax.inject.Inject;
+
+public class SimpleClanEnemyService
+        extends AbstractDomainClanService<ClanEnemyData>
+        implements ClanEnemyService {
+
+    @Inject private GlobalNotifier globalNotifier;
+
+    public SimpleClanEnemyService() {
+        super(ClanEnemyData::create, "enemy-list");
+    }
+
+    @Override
+    public void addEnemy(
+            Player sender, User user,
+            ClanMemberData targetMemberData
+    ) {
+        memberService.computeAsOwner(
+                sender, user,
+                memberData -> {
+                    String tag = memberData.getId();
+                    ClanEnemyData enemyData = getData(sender, tag);
+
+                    if (enemyData == null) {
+                        return;
+                    }
+
+                    String targetTag = targetMemberData.getId();
+
+                    if (enemyData.contains(targetTag)) {
+                        messageHandler.send(sender, "clan.already-enemy");
+                        return;
+                    }
+
+                    enemyData.add(targetTag);
+
+                    globalNotifier.notify(
+                            memberData.getOnlineIdMembers(),
+                            "clan.enemy-added-members",
+                            "%tag%", targetTag
+                    );
+
+                    globalNotifier.notify(
+                            targetMemberData.getOnlineIdMembers(),
+                            "clan.enemy-added-target-members",
+                            "%tag%", tag
+                    );
+
+                    save(sender, enemyData);
+                }
+        );
+    }
+
+    @Override
+    public void removeEnemy(
+            Player sender, User user,
+            ClanMemberData targetMemberData
+    ) {
+        memberService.computeAsOwner(
+                sender, user,
+                memberData -> {
+                    String tag = memberData.getId();
+                    ClanEnemyData enemyData = getData(sender, tag);
+
+                    if (enemyData == null) {
+                        return;
+                    }
+
+                    String targetTag = targetMemberData.getId();
+
+                    if (!enemyData.contains(targetTag)) {
+                        messageHandler.send(sender, "clan.not-enemy");
+                        return;
+                    }
+
+                    enemyData.remove(targetTag);
+
+                    globalNotifier.notify(
+                            memberData.getOnlineIdMembers(),
+                            "clan.enemy-removed-members",
+                            "%tag%", targetTag
+                    );
+
+                    globalNotifier.notify(
+                            targetMemberData.getOnlineIdMembers(),
+                            "clan.enemy-removed-target-members",
+                            "%tag%", tag
+                    );
+
+                    save(sender, enemyData);
+                }
+        );
+    }
+}
