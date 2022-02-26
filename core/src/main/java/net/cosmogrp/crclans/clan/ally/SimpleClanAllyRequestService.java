ba90e014a9bd6a13847cf68c43b46438c5551d6e
pixeldev <pixeldev1729@gmail.com>
2022-02-26 10:30:39 -0600

    feat(clan-ally): implement accept and deny ally request logic

--- core/src/main/java/net/cosmogrp/crclans/clan/ally/SimpleClanAllyRequestService.java
+++ core/src/main/java/net/cosmogrp/crclans/clan/ally/SimpleClanAllyRequestService.java
@@ -9,6 +9,7 @@ import net.cosmogrp.crclans.time.TimeStamp;
 import net.cosmogrp.crclans.user.User;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.Nullable;
 
 import javax.inject.Inject;
 
@@ -121,17 +122,124 @@ public class SimpleClanAllyRequestService
 
     @Override
     public void acceptAlly(
-            Player player, String target,
-            ClanAllyRequestData data
+            Player player, User user,
+            ClanAllyRequestData requestData
     ) {
+        memberService.computeAsOwner(
+                player, user,
+                targetMemberData -> {
+                    String targetTag = targetMemberData.getId();
+                    TimeStamp timeStamp = checkRequest(
+                            requestData, targetTag, player
+                    );
 
+                    if (timeStamp == null) {
+                        return;
+                    }
+
+                    String senderTag = requestData.getId();
+
+                    ClanMemberData senderMemberData = memberService.getData(
+                            player, senderTag
+                    );
+
+                    if (senderMemberData == null) {
+                        return;
+                    }
+
+                    requestData.unbind(targetTag);
+
+                    globalNotifier.notify(
+                            targetMemberData.getOnlineIdMembers(),
+                            "clan.ally-accept-target-members",
+                            "%tag%", senderTag
+                    );
+
+                    globalNotifier.notify(
+                            senderMemberData.getOnlineIdMembers(),
+                            "clan.ally-accept-members",
+                            "%tag%", targetTag
+                    );
+
+                    ClanAllyData clanAllyData = allyService.getData(senderTag);
+                    ClanAllyData targetAllyData = allyService.getData(targetTag);
+
+                    if (clanAllyData == null || targetAllyData == null) {
+                        return;
+                    }
+
+                    clanAllyData.addAlly(targetTag);
+                    targetAllyData.addAlly(senderTag);
+
+                    save(player, requestData);
+                    allyService.save(player, clanAllyData);
+                    allyService.save(player, targetAllyData);
+                }
+        );
     }
 
     @Override
     public void denyAlly(
-            Player player, String target,
-            ClanAllyRequestData data
+            Player player, User user,
+            ClanAllyRequestData requestData
     ) {
+        memberService.computeAsOwner(
+                player, user,
+                targetMemberData -> {
+                    String targetTag = targetMemberData.getId();
+                    TimeStamp timeStamp = checkRequest(
+                            requestData, targetTag, player
+                    );
 
+                    if (timeStamp == null) {
+                        return;
+                    }
+
+                    String senderTag = requestData.getId();
+                    ClanMemberData senderMemberData = memberService.getData(
+                            player, senderTag
+                    );
+
+                    if (senderMemberData == null) {
+                        return;
+                    }
+
+                    requestData.unbind(targetTag);
+                    globalNotifier.notify(
+                            senderMemberData.getOnlineIdMembers(),
+                            "clan.ally-deny-members",
+                            "%tag%", targetTag
+                    );
+
+                    globalNotifier.notify(
+                            targetMemberData.getOnlineIdMembers(),
+                            "clan.ally-deny-target-members",
+                            "%tag%", senderTag
+                    );
+
+                    save(player, requestData);
+                }
+        );
+    }
+
+    private @Nullable TimeStamp checkRequest(
+            ClanAllyRequestData requestData,
+            String tag, Player player
+    ) {
+        TimeStamp timeStamp = requestData.getTimestamp(tag);
+
+        if (timeStamp == null) {
+            messageHandler.send(player, "clan.ally-no-request");
+            return null;
+        }
+
+        if (timeStamp.hasExpired()) {
+            requestData.unbind(tag);
+            messageHandler.send(player, "clan.ally-request-expired");
+            save(player, requestData);
+            return null;
+        }
+
+        return timeStamp;
     }
 }
