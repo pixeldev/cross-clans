717e50a7dd59bc61bb0c451a9fc2471441b03bc2
pixeldev <pixeldev1729@gmail.com>
2022-02-25 23:14:21 -0600

    refactor(command): implement it as a clan service part

--- core/src/main/java/net/cosmogrp/crclans/command/internal/ClanPartModule.java
+++ core/src/main/java/net/cosmogrp/crclans/command/internal/ClanPartModule.java
@@ -3,12 +3,15 @@ package net.cosmogrp.crclans.command.internal;
 import me.fixeddev.commandflow.annotated.part.AbstractModule;
 import me.fixeddev.commandflow.annotated.part.Key;
 import me.fixeddev.commandflow.bukkit.annotation.Sender;
+import net.cosmogrp.crclans.clan.ally.ClanAllyRequestData;
+import net.cosmogrp.crclans.clan.ally.ClanAllyRequestService;
 import net.cosmogrp.crclans.clan.channel.ClanChannel;
 import net.cosmogrp.crclans.clan.member.ClanMember;
 import net.cosmogrp.crclans.clan.recruitment.ClanRecruitmentData;
+import net.cosmogrp.crclans.clan.recruitment.ClanRecruitmentService;
 import net.cosmogrp.crclans.command.part.ClanChannelPart;
 import net.cosmogrp.crclans.command.part.ClanMemberPart;
-import net.cosmogrp.crclans.command.part.ClanRecruitmentPart;
+import net.cosmogrp.crclans.command.part.ClanServicePart;
 import net.cosmogrp.crclans.command.part.ClusteredUserPart;
 import net.cosmogrp.crclans.command.part.UserPart;
 import net.cosmogrp.crclans.command.part.UserSenderPart;
@@ -22,15 +25,18 @@ public class ClanPartModule extends AbstractModule {
     @Inject private ClusteredUserPart clusteredUserPart;
     @Inject private UserPart userPart;
     @Inject private UserSenderPart userSenderPart;
-    @Inject private ClanRecruitmentPart clanRecruitmentPart;
     @Inject private ClanChannelPart clanChannelPart;
     @Inject private ClanMemberPart clanMemberPart;
+
+    @Inject private ClanRecruitmentService recruitmentService;
+    @Inject private ClanAllyRequestService allyRequestService;
 
     @Override
     public void configure() {
         bindFactory(ClusteredUser.class, clusteredUserPart);
         bindFactory(User.class, userPart);
-        bindFactory(ClanRecruitmentData.class, clanRecruitmentPart);
+        bindFactory(ClanRecruitmentData.class, new ClanServicePart<>(recruitmentService));
+        bindFactory(ClanAllyRequestData.class, new ClanServicePart<>(allyRequestService));
         bindFactory(ClanMember.class, clanMemberPart);
         bindFactory(ClanChannel.class, clanChannelPart);
         bindFactory(new Key(User.class, Sender.class), userSenderPart);
