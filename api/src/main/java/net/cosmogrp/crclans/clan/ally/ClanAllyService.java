f0daf8140fc954c0ffcd36be75eaa724d0091b1b
pixeldev <pixeldev1729@gmail.com>
2022-02-26 16:20:34 -0600

    refactor(clan-ally): use sendList

--- api/src/main/java/net/cosmogrp/crclans/clan/ally/ClanAllyService.java
+++ api/src/main/java/net/cosmogrp/crclans/clan/ally/ClanAllyService.java
@@ -1,14 +1,12 @@
 package net.cosmogrp.crclans.clan.ally;
 
-import net.cosmogrp.crclans.clan.service.ClanService;
+import net.cosmogrp.crclans.clan.service.DomainClanService;
 import net.cosmogrp.crclans.user.User;
 import org.bukkit.entity.Player;
 
-public interface ClanAllyService extends ClanService<ClanAllyData> {
+public interface ClanAllyService extends DomainClanService<ClanAllyData> {
 
     String KEY = "clan-ally";
-
-    void sendAllies(Player sender, User user);
 
     void removeAlly(
             Player sender, User user,
--- core/src/main/java/net/cosmogrp/crclans/clan/ally/SimpleClanAllyService.java
+++ core/src/main/java/net/cosmogrp/crclans/clan/ally/SimpleClanAllyService.java
@@ -1,64 +1,21 @@
 package net.cosmogrp.crclans.clan.ally;
 
-import net.cosmogrp.crclans.clan.service.AbstractClanService;
 import net.cosmogrp.crclans.clan.member.ClanMemberData;
-import net.cosmogrp.crclans.clan.member.ClanMemberService;
+import net.cosmogrp.crclans.clan.service.AbstractDomainClanService;
 import net.cosmogrp.crclans.notifier.global.GlobalNotifier;
 import net.cosmogrp.crclans.user.User;
 import org.bukkit.entity.Player;
 
 import javax.inject.Inject;
-import java.util.Collection;
 
 public class SimpleClanAllyService
-        extends AbstractClanService<ClanAllyData>
+        extends AbstractDomainClanService<ClanAllyData>
         implements ClanAllyService {
 
-    @Inject private ClanMemberService memberService;
     @Inject private GlobalNotifier globalNotifier;
 
     public SimpleClanAllyService() {
-        super(ClanAllyData::create);
-    }
-
-    @Override
-    public void sendAllies(Player sender, User user) {
-        String tag = memberService.getClanTag(sender, user);
-
-        if (tag == null) {
-            return;
-        }
-
-        ClanAllyData allyData = getData(sender, tag);
-
-        if (allyData == null) {
-            return;
-        }
-
-        Collection<String> allies = allyData.getAll();
-
-        if (allies.isEmpty()) {
-            messageHandler.send(sender, "clan.ally-list.no-allies");
-        } else {
-            StringBuilder formattedAllies = new StringBuilder();
-            int i = 0;
-
-            for (String ally : allies) {
-                formattedAllies.append(messageHandler.replacing(
-                        sender, "clan.ally-list.ally-format",
-                        "%tag%", ally
-                ));
-
-                if (i < allies.size() - 1) {
-                    formattedAllies.append("\n");
-                }
-            }
-
-            messageHandler.sendReplacing(
-                    sender, "clan.ally-list.message",
-                    "%allies%", formattedAllies.toString()
-            );
-        }
+        super(ClanAllyData::create, "ally-list");
     }
 
     @Override
