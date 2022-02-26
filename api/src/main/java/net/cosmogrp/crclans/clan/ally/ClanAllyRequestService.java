653de9704979027d40f0eabb3e0403c36257d1f7
pixeldev <pixeldev1729@gmail.com>
2022-02-26 09:30:10 -0600

    feat(clan): implement clan ally request micro service

---
+++ api/src/main/java/net/cosmogrp/crclans/clan/ally/ClanAllyRequestService.java
@@ -0,0 +1,25 @@
+package net.cosmogrp.crclans.clan.ally;
+
+import net.cosmogrp.crclans.clan.ClanService;
+import net.cosmogrp.crclans.user.User;
+import org.bukkit.entity.Player;
+
+public interface ClanAllyRequestService
+        extends ClanService<ClanAllyRequestData> {
+
+    void sendAllyRequest(
+            Player player, User user,
+            String target
+    );
+
+    void acceptAlly(
+            Player player, String target,
+            ClanAllyRequestData data
+    );
+
+    void denyAlly(
+            Player player, String target,
+            ClanAllyRequestData data
+    );
+
+}
