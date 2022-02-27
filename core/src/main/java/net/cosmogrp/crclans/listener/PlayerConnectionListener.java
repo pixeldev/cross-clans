2385452394c9628231780a625a0f2419fb267db7
pixeldev <pixeldev1729@gmail.com>
2022-02-27 11:43:34 -0600

    refactor(user): use player service to load and save player

--- core/src/main/java/net/cosmogrp/crclans/listener/PlayerConnectionListener.java
+++ core/src/main/java/net/cosmogrp/crclans/listener/PlayerConnectionListener.java
@@ -1,59 +1,25 @@
 package net.cosmogrp.crclans.listener;
 
-import net.cosmogrp.crclans.server.ServerSender;
-import net.cosmogrp.crclans.user.User;
-import net.cosmogrp.crclans.user.clan.ClanUserService;
-import net.cosmogrp.crclans.user.cluster.ClusteredUserRegistry;
-import net.cosmogrp.crclans.user.UserService;
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
+import net.cosmogrp.crclans.user.PlayerService;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.plugin.Plugin;
 
 import javax.inject.Inject;
 
 public class PlayerConnectionListener implements Listener {
 
-    @Inject private Plugin plugin;
-
-    @Inject private UserService userService;
-    @Inject private ClanUserService clanUserService;
-    @Inject private ClusteredUserRegistry clusteredUserRegistry;
-    @Inject private ServerSender serverSender;
+    @Inject private PlayerService playerService;
 
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
-        Player player = event.getPlayer();
-
-        Bukkit.getScheduler().runTaskLater(
-                plugin,
-                () -> {
-                    userService.loadOrCreate(player)
-                            .whenComplete((user, throwable) -> {
-                                if (user != null) {
-                                    clanUserService.connect(player, user);
-                                    clusteredUserRegistry.create(player);
-                                }
-                            });
-
-                    serverSender.checkTeleport(player);
-                },
-                20L
-        );
+        playerService.load(event.getPlayer());
     }
 
     @EventHandler
     public void onQuit(PlayerQuitEvent event) {
-        Player player = event.getPlayer();
-        User user = userService.saveUser(player);
-        clusteredUserRegistry.delete(player);
-
-        if (user != null) {
-            clanUserService.disconnect(user);
-        }
+        playerService.save(event.getPlayer());
     }
 
 }
