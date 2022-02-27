93fe6be16ddb7d558bd1eb9f0e39b4ee9261cbce
pixeldev <pixeldev1729@gmail.com>
2022-02-26 21:45:44 -0600

    feat(command): add getSuggestions to get domain suggestions

--- core/src/main/java/net/cosmogrp/crclans/command/part/PartHelper.java
+++ core/src/main/java/net/cosmogrp/crclans/command/part/PartHelper.java
@@ -2,11 +2,21 @@ package net.cosmogrp.crclans.command.part;
 
 import me.fixeddev.commandflow.CommandContext;
 import me.fixeddev.commandflow.bukkit.BukkitCommandManager;
+import me.fixeddev.commandflow.stack.ArgumentStack;
+import net.cosmogrp.crclans.clan.service.ClanService;
+import net.cosmogrp.crclans.domain.Domain;
 import net.cosmogrp.crclans.user.User;
 import net.cosmogrp.crclans.user.UserService;
+import net.cosmogrp.storage.model.Model;
+import net.cosmogrp.storage.mongo.DocumentCodec;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
 
 public final class PartHelper {
 
@@ -27,6 +37,38 @@ public final class PartHelper {
         }
 
         return userService.getUser(player.getUniqueId());
+    }
+
+    public static <T extends Model & DocumentCodec & Domain>
+    List<String> getSuggestions(
+            UserService userService,
+            CommandContext context,
+            ClanService<T> service,
+            ArgumentStack stack
+    ) {
+        String clanTag = PartHelper.getClanTag(userService, context);
+
+        if (clanTag == null) {
+            return Collections.emptyList();
+        }
+
+        T data = service.getData(clanTag);
+
+        if (data == null) {
+            return Collections.emptyList();
+        }
+
+        String next = !stack.hasNext() ? "" :
+                stack.next().toLowerCase(Locale.ROOT);
+        List<String> suggestions = new ArrayList<>();
+
+        for (String allyTag : data.getAll()) {
+            if (allyTag.toLowerCase(Locale.ROOT).startsWith(next)) {
+                suggestions.add(allyTag);
+            }
+        }
+
+        return suggestions;
     }
 
     public static @Nullable String getClanTag(
