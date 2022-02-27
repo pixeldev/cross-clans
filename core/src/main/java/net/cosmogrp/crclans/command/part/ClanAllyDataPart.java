ce60bc2db39491b3a141e16546c6dff70d651847
pixeldev <pixeldev1729@gmail.com>
2022-02-26 21:46:39 -0600

    refactor(command): use helper to get suggestions

--- core/src/main/java/net/cosmogrp/crclans/command/part/ClanAllyDataPart.java
+++ core/src/main/java/net/cosmogrp/crclans/command/part/ClanAllyDataPart.java
@@ -6,10 +6,7 @@ import net.cosmogrp.crclans.clan.ally.ClanAllyData;
 import net.cosmogrp.crclans.clan.service.ClanService;
 import net.cosmogrp.crclans.user.UserService;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Locale;
 
 public class ClanAllyDataPart extends ClanServicePart<ClanAllyData> {
 
@@ -29,28 +26,11 @@ public class ClanAllyDataPart extends ClanServicePart<ClanAllyData> {
             CommandContext commandContext,
             ArgumentStack stack
     ) {
-        String clanTag = PartHelper.getClanTag(userService, commandContext);
-
-        if (clanTag == null) {
-            return Collections.emptyList();
-        }
-
-        ClanAllyData allyData = clanService.getData(clanTag);
-
-        if (allyData == null) {
-            return Collections.emptyList();
-        }
-
-        String next = !stack.hasNext() ? "" :
-                stack.next().toLowerCase(Locale.ROOT);
-        List<String> suggestions = new ArrayList<>();
-
-        for (String allyTag : allyData.getAll()) {
-            if (allyTag.toLowerCase(Locale.ROOT).startsWith(next)) {
-                suggestions.add(allyTag);
-            }
-        }
-
-        return suggestions;
+        return PartHelper.getSuggestions(
+                userService,
+                commandContext,
+                clanService,
+                stack
+        );
     }
 }
